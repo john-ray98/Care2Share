30b6708542406e0564b31d738f6df5b66738ec8f
John Ray <jray22@msu.idserve.net>
2023-05-02 16:08:02 -0400

    Created a MessageBoardView

---
+++ Care2Share/MessageBoardView .swift
@@ -0,0 +1,20 @@
+//
+//  MessageBoardView .swift
+//  Care2Share
+//
+//  Created by John Ray on 5/2/23.
+//
+
+import SwiftUI
+
+struct MessageBoardView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageBoardView__Previews: PreviewProvider {
+    static var previews: some View {
+        MessageBoardView_()
+    }
+}
