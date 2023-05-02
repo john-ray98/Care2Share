4ecc0ec7a17be2b8f8b9c07cbac2139fc015583d
John Ray <jray22@msu.idserve.net>
2023-05-02 15:41:54 -0400

    Initial Commit

---
+++ Care2Share/Care2ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Care2ShareApp.swift
+//  Care2Share
+//
+//  Created by John Ray on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Care2ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Care2Share/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Care2Share
+//
+//  Created by John Ray on 5/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
