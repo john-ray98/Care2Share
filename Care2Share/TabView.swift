ecf7761ffb257f61a75b821156b701d17bf5b547
John Ray <jray22@msu.idserve.net>
2023-05-02 15:59:00 -0400

    Created a TabView

---
+++ Care2Share/TabView.swift
@@ -0,0 +1,20 @@
+//
+//  TabView.swift
+//  Care2Share
+//
+//  Created by John Ray on 5/2/23.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView()
+    }
+}
