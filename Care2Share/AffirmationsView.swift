53de84f5f53fb7cd856c17bac720aeb526f685c7
John Ray <jray22@msu.idserve.net>
2023-05-04 13:01:25 -0400

    modified MessageBoardView to toggle between AffirmationsView screen and default MessageBoardView.

---
+++ Care2Share/AffirmationsView.swift
@@ -0,0 +1,20 @@
+//
+//  AffirmationsView.swift
+//  Care2Share
+//
+//  Created by John Ray on 5/3/23.
+//
+
+import SwiftUI
+
+struct AffirmationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AffirmationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AffirmationsView()
+    }
+}
--- Care2Share/MessageBoardView .swift
+++ Care2Share/MessageBoardView .swift
@@ -7,28 +7,82 @@
 
 import SwiftUI
 
+enum ScreenType: String, CaseIterable {
+    case message = "Messages"
+    case affirmations = "Affirmations"
+}
+
 struct MessageBoardView_: View {
-   @State private var defaulted = "Message"
     
-    let boards = ["Message", "Affirmations"]
-    
+    /* @State private var defaulted = "Message"
+     @State private var clickOver = "Affirmations"
+     let boards = ["Message", "Affirmations"]*/
+    @State private var selectedScreen: ScreenType = .message
     var body: some View {
-        //create navigation links for access to separate pages
         NavigationView {
             Form {
                 Section {
-                    Picker("Board", selection: $defaulted)
-                    { ForEach(boards, id: \.self)
-                        {
-                            Text($0)
+                    VStack {
+                        Picker(selection: $selectedScreen, label: Text("Screen Type")) {
+                            ForEach(ScreenType.allCases, id: \.self) { screen in
+                                Text(screen.rawValue)
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                        
+                        switch selectedScreen {
+                        case .message:
+                            MessageView()
+                        case .affirmations:
+                            AffirmationsView()
                         }
-                    }.pickerStyle(.segmented)
+                    }
+                    .navigationTitle(selectedScreen.rawValue)
                 }
-                .navigationTitle("Message Boards")
             }
         }
     }
 }
+
+struct MessageView: View {
+    var body: some View {
+        MessageBoardView_()
+    }
+}
+
+struct AffirmationView: View {
+    var body: some View {
+      AffirmationView()
+    }
+}
+    
+
+        
+        
+        
+        //create navigation links for access to separate pages
+       /* NavigationView {
+            if defaulted == "Affirmations" {
+                AffirmationsView()
+            } else {
+                
+                
+                
+                Form {
+                    Section {
+                        Picker("Board", selection: $defaulted)
+                        { ForEach(boards, id: \.self)
+                            {
+                                Text($0)
+                            }
+                        }.pickerStyle(.segmented)
+                    }
+                }
+                .navigationTitle("Message Boards")
+            }*/
+        
+    
+
+
             struct MessageBoardView__Previews: PreviewProvider {
                 static var previews: some View {
                     MessageBoardView_()
